ada96ab3e0ac79fcdc783b54ba6635298a0a50be
zhoujz <1371634433@qq.com>
2026-03-09 00:45:34 +0800

    init

---
+++ src/agent.js
@@ -0,0 +1,45 @@
+import OpenAI from "openai";
+
+const baseURL = "https://api.deepseek.com";
+const apiKey = "sk-d45bd5a916964cf4a6d124b7eb6b9674";
+
+const openai = new OpenAI({
+  baseURL,
+  apiKey,
+});
+
+export async function getUserAction(input) {
+  const systemPrompt = `
+你是一个前端Agent，负责将用户指令转换为JSON动作。
+可用的动作包括:
+1. showProjectList：查看项目列表，参数 type，取值可以是"my"、"starred"、"all"；
+2. showTaskList：查看任务列表，参数 type，取值可以是"recent"、"global";
+3. createProject：创建项目。
+
+示例：
+输入：列出我收藏的项目
+输出：
+{
+    "action":"showProjectList",
+    "type":"starred"
+}
+
+如果无法匹配动作，返回:
+{
+  "action": "unknown"
+}
+`;
+  const messages = [
+    { role: "system", content: systemPrompt },
+    { role: "user", content: input },
+  ];
+  const completion = await openai.chat.completions.create({
+    messages,
+    model: "deepseek-chat",
+    response_format: {
+      type: "json_object",
+    },
+  });
+  const res = completion.choices[0].message.content;
+  return res;
+}
---
+++ src/index.js
@@ -0,0 +1,65 @@
+import express from "express";
+import { getUserAction } from "./agent.js";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api/projects", (req, res) => {
+  const { type } = req.query;
+  const projectListData = [
+    {
+      key: "1",
+      name: "Jane Doe",
+      salary: 23000,
+      address: "32 Park Road, London",
+      email: "jane.doe@example.com",
+    },
+    {
+      key: "2",
+      name: "Alisa Ross",
+      salary: 25000,
+      address: "35 Park Road, London",
+      email: "alisa.ross@example.com",
+    },
+    {
+      key: "3",
+      name: "Kevin Sandra",
+      salary: 22000,
+      address: "31 Park Road, London",
+      email: "kevin.sandra@example.com",
+    },
+    {
+      key: "4",
+      name: "Ed Hellen",
+      salary: 17000,
+      address: "42 Park Road, London",
+      email: "ed.hellen@example.com",
+    },
+    {
+      key: "5",
+      name: "William Smith",
+      salary: 27000,
+      address: "62 Park Road, London",
+      email: "william.smith@example.com",
+    },
+  ];
+  res.send(projectListData);
+});
+
+app.post("/api/agent", async (req, res) => {
+  const { input } = req.body;
+  console.log(input);
+
+  // const action = await getUserAction(input);
+  // res.send(action);
+});
+
+app.listen(3000, () => {
+  console.log("服务已启动...");
+});
